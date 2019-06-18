78fd3e0648c1774640c052074e58eaf83fe094e9
brellin <cekakid@gmail.com>
2019-06-18 12:35:36 -0500

    add '/auth/login' endpoint and update readme

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 ### Auth
 Request | Endpoint | Required Fields | Description
 --------|----------|-----------------|------------
-POST | `/auth/register` | { username, password } | Accepts a username and password, checks against existing usernames, and returns an auth object if all goes right
+POST | `/auth/register` | { username, password } | Accepts a username and password, checks against existing usernames, and returns an auth object if user doesn't exist OR if fields match
+POST | `/auth/login` | { username, password } | Accepts a username and password, checks against existing usernames, and returns an auth object if fields match
 #### Auth object shape
 ```
 {
--- server/auth/index.js
+++ server/auth/index.js
@@ -5,6 +5,7 @@ const { makeToken } = require('./middleware')
 const Users = require('../actions')
 
 router.post('/register', async (req, res) => {
+
     let { username, password } = req.body
 
     if (req.body && username && password) {
@@ -30,8 +31,9 @@ router.post('/register', async (req, res) => {
                     const token = makeToken(check)
 
                     res.status(200).json({
-                        username,
+                        message: `Welcome, ${username}!`,
                         id: check.id,
+                        username,
                         token
                     })
 
@@ -60,6 +62,51 @@ router.post('/register', async (req, res) => {
             error: 'You must include a username and password in your request.'
         })
 
+    }
+
+})
+
+router.post('/login', async (req, res) => {
+
+    let { username, password } = req.body
+
+    if (req.body && username && password) {
+
+        try {
+
+            const check = await Users.find('users', username)
+
+            if (check && bcrypt.compareSync(password, check.password)) {
+
+                const token = makeToken(check)
+
+                res.status(200).json({
+                    message: `Welcome, ${username}!`,
+                    id: check.id,
+                    username,
+                    token
+                })
+
+            } else {
+                res.status(401).json({
+                    error: 'Invalid username and/or password.'
+                })
+            }
+
+        } catch (err) {
+
+            console.log(err)
+
+            res.status(500).json({
+                error: 'Internal Server Error'
+            })
+
+        }
+
+    } else {
+        res.status(400).json({
+            error: 'You must include a username and password in your request.'
+        })
     }
 
 })
