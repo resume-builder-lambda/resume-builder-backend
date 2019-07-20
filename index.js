e99f90b41db81fe6f8c765c65c9cdbcfc43e59dc
brellin <cekakid@gmail.com>
2019-07-20 00:37:19 -0500

    attempt combo backend remove access control headers

--- index.js
+++ index.js
@@ -68,10 +68,7 @@ server.post('/auth/linkedin', cors(), (req, res) => {
     fetch(`https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=${code}&redirect_uri=${process.env.REDIRECT_REGISTER_URI}&client_id=${process.env.LINKEDIN_CLIENT_ID}&client_secret=${process.env.LINKEDIN_CLIENT_SECRET}`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type'
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
     })
         .then(res => res.status(200).json(res))
